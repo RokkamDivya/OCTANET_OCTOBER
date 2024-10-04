ead74911497e987491471a630bb42eaf405996d8
RokkamDivya <rokkamdivya7@gmail.com>
2024-10-04 11:31:55 +0530

    Add files via upload

---
+++ Task-1 Landing page/script.js
@@ -0,0 +1,10 @@
+// Smooth Scroll for Navigation Links
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+  });
+  
+  
